2ff139a8ade930d37b30136cb0d609eea7004605
rodica-andronache <rodik_andronache@yahoo.com>
2015-04-02 15:36:10 +0300

    Fixed #2, added prefixes for functions

--- functions.php
+++ functions.php
@@ -1,28 +1,26 @@
 <?php
-// add any new or customised functions here
 
-add_action( 'wp_enqueue_scripts', 'juridica_enqueue_styles' );
-function juridica_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'illyria_enqueue_styles' );
+function illyria_enqueue_styles() {
 
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	// Loads our main stylesheet.
-	wp_enqueue_style( 'juridica-child-style', get_stylesheet_uri(), array('parent-style'), 'v1' );
+	wp_enqueue_style( 'illyria-child-style', get_stylesheet_uri(), array('parent-style'), 'v1' );
 
 }
 
-function juridica_custom_script_fix()
+function illyria_custom_script_fix()
 {
    
-	wp_enqueue_script('juridica_script_child',get_stylesheet_directory_uri().'/js/wrapall.js', array('jquery') );
+	wp_enqueue_script('illyria_script_child',get_stylesheet_directory_uri().'/js/wrapall.js', array('jquery') );
 	
 }
 
-add_action( 'wp_enqueue_scripts', 'juridica_custom_script_fix', 100 );
+add_action( 'wp_enqueue_scripts', 'illyria_custom_script_fix', 100 );
 
-function juridica_inline_styles()
-		{
-		?>
-		<style type="text/css">
+function illyria_inline_styles() {
+	?>
+	<style type="text/css">
 
 		<?php if(is_front_page()): ?>
 		.wrap-elements {
@@ -33,8 +31,8 @@ function juridica_inline_styles()
 		 position: relative !important;
 		}
 		<?php endif; ?>
-		</style>
-		<?php
-		}
+	</style>
+	<?php
+}
 
-add_action("wp_print_scripts","juridica_inline_styles");
+add_action("wp_print_scripts","illyria_inline_styles");
